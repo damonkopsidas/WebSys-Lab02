40f5886573be51c4c4b5d82248f1fcf34c14aef4
Damon Kopsidas <1713268@rgu.ac.uk>
2022-01-25 15:35:43 +0000

    Adding port const and stuff

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 http.createServer(function (request, response){
     // Send the HTTP header
     // HTTP Status: 200 : OK
@@ -8,7 +10,7 @@ http.createServer(function (request, response){
 
     //Send the response body as "Hello World"
     response.end('Hello World\n');
-}).listen(8080);
+}).listen(PORT);
 
 //Console will print the message
-console.log('Server running at http://127.0.0.1:8080/');
+console.log(`Server running at http://127.0.0.1:${PORT}/`);

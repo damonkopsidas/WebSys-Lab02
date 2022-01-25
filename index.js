a6050554079e1a7ea1e28567530aa9a3077bf034
Damon Kopsidas <1713268@rgu.ac.uk>
2022-01-25 15:56:49 +0000

    html serving update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var http = require("http");
-
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response){
     // Send the HTTP header
     // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Content Type: text/html
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin' : '*'
+    });
+    var readStream = fs.createReadStream(__dirname + '/index.html');
 
     //Send the response body as "Hello World"
-    response.end('Hello World\n');
+    readStream.pipe(response);
 }).listen(PORT);
 
 //Console will print the message

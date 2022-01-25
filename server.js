b1ecb8ddf06929c5209433ac5cb1a8bbe49d1678
Damon Kopsidas <1713268@rgu.ac.uk>
2022-01-25 16:52:09 +0000

    ye

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/', function(req, res){
 // create routes for the admin section
 //get an instance of the router
 var adminRouter = express.Router();
+
+//route middleware that will happen on every request
+adminRouter.use(function(req, res, next){
+    //log each request to the console
+    console.log(req.method, req.url);
+    //continue doing what we were doing and go to route
+    next();
+});
 // admin main page. the dashboard (http://localhost:PORT/admin)
 adminRouter.get('/', function(req, res){
     res.send('ٱلْحَمْدُ لِلَّٰهِ'); });
